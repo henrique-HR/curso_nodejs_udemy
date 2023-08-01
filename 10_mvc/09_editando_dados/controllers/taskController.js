fe285fba9aae3b9965e999ab1ae64a1d8a2e7df3
henrique-HR <99933690+henrique-HR@users.noreply.github.com>
2023-07-31 23:13:31 -0300

    atualizacao

---
+++ 10_mvc/09_editando_dados/controllers/taskController.js
@@ -0,0 +1,42 @@
+const Task = require('../models/Task')
+
+module.exports = class TaskController{
+
+    static createTask(req,res){
+        res.render('tasks/create')
+    }
+    static async createTasksave(req,res){
+        const task = {
+            title: req.body.title,
+            description: req.body.description,
+            done: false
+        } 
+        //validaçoes 
+        // processar dados  
+       await Task.create(task)
+       res.redirect('/tasks')
+    }
+    static async showTask(req,res){
+        const task =  await Task.findAll({raw: true})
+        
+        res.render('tasks/show',{task: task})
+    } 
+    static async  removeTask(req,res){
+
+        const id = req.body.id 
+        
+        await Task.destroy({where:{id:id}})
+
+        res.redirect('/tasks')
+    }
+    static async updateTask(req,res){ 
+        const id = req.parans.id
+
+        const task = await Task.findOne({where:{id:id}})
+ 
+
+        res.render(`tasks/edit`,{task})
+    }
+
+
+}  
---
+++ 10_mvc/09_editando_dados/routes/taskRoutes.js
@@ -0,0 +1,18 @@
+
+const express = require('express')
+const router = express.Router()
+
+const taskController = require('../controllers/taskController')
+ 
+router.get('/add', taskController.createTask)
+
+router.post('/add', taskController.createTasksave)
+
+router.get('/', taskController.showTask) 
+
+router.get(`/edit/:id`,taskController.updateTask)
+
+router.post('/remove',taskController.removeTask)
+
+
+module.exports = router
